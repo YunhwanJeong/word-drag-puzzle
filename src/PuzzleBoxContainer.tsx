3df3a04d2ae0823d2c881fc51dc8aa8b6ee0479f
yunhwan <jimmy53120488@gmail.com>
2022-01-05 00:14:47 +0900

    feat: random-words로 생성한 단어를 character로 split 후 shuffle하는 유틸 함수 추가

--- src/PuzzleBoxContainer.tsx
+++ src/PuzzleBoxContainer.tsx
@@ -2,12 +2,13 @@ import React, {useEffect, useState} from 'react';
 import './PuzzleBoxContainer.css';
 import randomWords from 'random-words';
 import Box from './Box';
+import {shuffleArray} from './Utils';
 
 function PuzzleBoxContainer() {
     const [characters, setCharacters] = useState<string[]>([]);
 
     useEffect(() => {
-        setCharacters(randomWords().split(''));
+        setCharacters(shuffleArray(randomWords().split('')));
     }, []);
 
     const boxes = characters.map((c, i) => <Box key={`${c}${i}`} character={c} />);
