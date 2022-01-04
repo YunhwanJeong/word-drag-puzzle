8deed73880f21fadf0ab6c5297b057c7618dc874
yunhwan <jimmy53120488@gmail.com>
2022-01-05 00:08:57 +0900

    chore: PuzzleAnswer의 유저 안내 문구 수정

--- src/PuzzleAnswer.tsx
+++ src/PuzzleAnswer.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './PuzzleAnswer.css';
 
-export const USER_GUIDE_KEYWORD = 'Drag & Drop the cards here';
+export const USER_GUIDE_KEYWORD = 'Drag & Drop cards here';
 
 function PuzzleAnswer() {
     return (
         <div className='puzzle-answer'>
-            <p>{`${USER_GUIDE_KEYWORD} to make phrases that make sense!`}</p>
+            <p>{`${USER_GUIDE_KEYWORD} to complete meaningful word.`}</p>
         </div>
     );
 }
